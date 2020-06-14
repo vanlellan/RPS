97b1764e0bfb6ccc5133fcc37d503f74e7430d5f
R Evan McClellan <vanlellan@gmail.com>
2020-06-14 10:49:25 -0500

    update tkinter demo

--- demos/tkinter-graphics.py
+++ demos/tkinter-graphics.py
@@ -3,17 +3,19 @@
 
 import tkinter as tk
 
-
+#ToDo:
+#   [ ] Use pressed dict to handle key press/release, rather than OS
 
 
 class demoScreen():
     def __init__(self):
         self.root = tk.Tk()
+        self.pressedDict = {'Left':False, 'Right':False, 'Up':False, 'Down':False}
         self.canvas = tk.Canvas(self.root, width=400, height=400)
         self.circle1 = self.canvas.create_oval(20,20,200,200,outline='black',fill='red')
         self.canvas.bind('<Escape>', self.bail)
-        self.canvas.bind('<Key>', self.printEvent)
-        self.canvas.bind('<Left>', self.moveLeft)
+        self.canvas.bind('<KeyPress>', self.printEvent)
+        self.canvas.bind('<KeyRelease>', self.printEvent)
         self.canvas.pack()
         self.canvas.focus_set()
         self.canvas.update()
@@ -23,10 +25,13 @@ class demoScreen():
         print("Got Here printEvent")
         position = "x={}, y={}".format(event.x, event.y)
         print(event.type, "event", position, event.keysym)
+        if event.keysym in ['Left', 'Right', 'Up', 'Down']:
+            self.move(self, event.keysym)
 
-    def moveLeft(self, event):
-        print("moveLeft")
-        self.canvas.move(self.circle1, -1, 0)
+    def move(self, event, aKey):
+        print("move")
+        keyMap = {'Left':(-1,0), 'Right':(1,0), 'Up':(0,-1), 'Down':(0,1)}
+        self.canvas.move(self.circle1, keyMap[aKey][0], keyMap[aKey][1])
         self.canvas.update()
 
     def bail(self, event):
