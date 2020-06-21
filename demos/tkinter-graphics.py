fffbdf1abf5a66f0f0aa7a0314a6e01fd1117461
R Evan McClellan <vanlellan@gmail.com>
2020-06-20 19:02:56 -0500

    move circle stuff into its own object

--- demos/tkinter-graphics.py
+++ demos/tkinter-graphics.py
@@ -9,20 +9,35 @@ import platform
 #   [ ] Use pressed dict to handle key press/release, rather than OS
 
 
+class circ():
+    def __init__(self, aScreen):
+        self.aScreen = aScreen
+        self.keyMap = {'Left':(-1,0), 'Right':(1,0), 'Up':(0,-1), 'Down':(0,1)}
+        self.circle1 = self.aScreen.canvas.create_oval(20,20,30,30,outline='black',fill='red')
+        self.draw()
+
+    def draw(self):
+        self.aScreen.canvas.move(self.circle1, self.keyMap['Right'][0]*self.aScreen.pressedDict['Right'], 0)     #right
+        self.aScreen.canvas.move(self.circle1, 0, self.keyMap['Up'][1]*self.aScreen.pressedDict['Up'])           #up
+        self.aScreen.canvas.move(self.circle1, self.keyMap['Left'][0]*self.aScreen.pressedDict['Left'], 0)       #left
+        self.aScreen.canvas.move(self.circle1, 0, self.keyMap['Down'][1]*self.aScreen.pressedDict['Down'])      #down
+        self.aScreen.canvas.update()
+        self.aScreen.canvas.after(10, self.draw)
+
+
+
 class demoScreen():
     def __init__(self):
         self.root = tk.Tk()
         self.pressedDict = {'Left':False, 'Right':False, 'Up':False, 'Down':False}
-        self.keyMap = {'Left':(-1,0), 'Right':(1,0), 'Up':(0,-1), 'Down':(0,1)}
         self.canvas = tk.Canvas(self.root, width=400, height=400)
-        self.circle1 = self.canvas.create_oval(20,20,30,30,outline='black',fill='red')
+        self.circ1 = circ(self)
         self.canvas.bind('<Escape>', self.bail)
         self.canvas.bind('<KeyPress>', self.printEvent)
         self.canvas.bind('<KeyRelease>', self.printEvent)
         self.canvas.pack()
         self.canvas.focus_set()
         self.canvas.update()
-        self.draw()
 
     def printEvent(self, event):
         position = "x={}, y={}".format(event.x, event.y)
@@ -30,14 +45,6 @@ class demoScreen():
             self.pressedDict[event.keysym] = True
         if str(event.type) == 'KeyRelease':
             self.pressedDict[event.keysym] = False
-
-    def draw(self):
-        self.canvas.move(self.circle1, self.keyMap['Right'][0]*self.pressedDict['Right'], 0)     #right
-        self.canvas.move(self.circle1, 0, self.keyMap['Up'][1]*self.pressedDict['Up'])           #up
-        self.canvas.move(self.circle1, self.keyMap['Left'][0]*self.pressedDict['Left'], 0)       #left
-        self.canvas.move(self.circle1, 0, self.keyMap['Down'][1]*self.pressedDict['Down'])      #down
-        self.canvas.update()
-        self.canvas.after(10, self.draw)
 
     def bail(self, event):
         self.root.destroy()
